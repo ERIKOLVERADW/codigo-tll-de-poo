a2f61f13727c17b09b02265fa4d4d4f8be144db4
ERIKOLVERADW <105740606+ERIKOLVERADW@users.noreply.github.com>
2022-05-17 12:24:04 -0500

    Add files via upload

---
+++ codigo tll de poo/data/data.js
@@ -0,0 +1,22 @@
+export const data = [
+    {
+      question: "Cual de las siguientes opciones no es un editor de codigo?",
+      choices: ["vim", "vscode", "emacs", "word"],
+      answer: "word",
+    },
+    {
+      question: "Que lenguaje no es orientado a Objetos",
+      choices: ["Java", "Haskell", "C++", "Python"],
+      answer: "Haskell",
+    },
+    {
+      question: "Quien pinto la monalisa?",
+      choices: ["Leonardo Da Vinci", "Picasso", "Monet", "Vicent Van Goh", 'Frida Khalo', 'Diego Rivera'],
+      answer: "Leonardo Da Vinci",
+    },
+    {
+      question: "Que comio ray hoy?",
+      choices: ["Leonardo Da Vinci", "Picasso", "Monet", "Vicent Van Goh", 'Frida Khalo', 'Diego Rivera'],
+      answer: "Leonardo Da Vinci",
+    },
+  ];
